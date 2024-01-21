af7435a1cf66a2576ca0d737d3bf22555c2088d4
Palak Singh <90079523+singhpalak18@users.noreply.github.com>
2024-01-21 23:38:19 +0530

    Update mentalhealth_lr.py

--- Models/mentalhealth_lr.py
+++ Models/mentalhealth_lr.py
@@ -40,3 +40,67 @@ print(duplicates)
 
 #REMOVE STOP WORDS
 
+def remove_stopwords(Text):
+    words = nltk.word_tokenize(Text)
+    words = [word for word in words if word.lower() not in stopwords.words("english")]
+    return " ".join(words)
+
+df['Text'] = df['Text'].apply(remove_stopwords)
+df.to_csv("mentaldataset_cleaned.csv", index=False)
+df.to_csv("mentaldataset_cleaned.csv",index=False)
+df.to_excel("mentaldataset_cleaned.xlsx",index=False)
+data=pd.read_csv("mentaldataset_cleaned.csv")
+tfidf_vectorizer = TfidfVectorizer(max_features=1000)
+X = tfidf_vectorizer.fit_transform(data['Text'])
+y = data['Label']
+
+#REGRESSION 
+
+tfidf_vectorizer = TfidfVectorizer(max_features=1000)
+X = tfidf_vectorizer.fit_transform(data['Text'])
+y = data['Label']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+classifier= LogisticRegression()
+classifier.fit(X, y)
+questions = [
+    "Do you ever worry about the security of your job?",
+"Do you feel confident at work? ",
+"Have you started to avoid spending time with friends and loved ones due to extensive work?",
+"Do you feel like you have a good work life balance here?",
+"Do you become irritable or annoyed more quickly than I have in the past?",
+"Do you often feel restless, on edge, or unable to relax?",
+"Do you feel comfortable talking about my mental health with others inside our organisation?",
+"Is it difficult to fall asleep, get enough sleep, or wake up on time most days?",
+"Do you ever feel overworked or underworked here as an employee?",
+"Do you feel that your work is not recognized or underappreciated?"
+]
+user_responses = []
+predicted_labels = []
+
+for i, question in enumerate(questions):
+    response = input(f"Q{i + 1}: {question} ")
+    user_responses.append(response)
+    response_tfidf = tfidf_vectorizer.transform([response])
+    predicted_label = classifier.predict(response_tfidf)[0]
+    predicted_labels.append(predicted_label)
+print("Predicted Labels for Each Question:")
+for i, label in enumerate(predicted_labels):
+    print(f"Q{i + 1}: {label}")
+
+y_pred = classifier.predict(X_test)
+classification_rep = classification_report(y_test, y_pred)
+
+print("\nClassification Report:")
+print(classification_rep)
+
+#DISPLAY STATS USING PIE CHART 
+
+label_counts = pd.Series(predicted_labels).value_counts()
+labels = label_counts.index
+sizes = label_counts.values
+colors=['purple', 'pink', 'cyan','red','blue','brown']
+plt.pie(sizes, labels=labels, colors=colors, startangle=90, shadow=True,explode=(0.1,)*len(labels), autopct='%1.2f%%')
+plt.axis('equal')
+
+plt.title('Composition of Labels for All Questions')
+plt.show()
